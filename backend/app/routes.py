c46f9596af920297a3523b9eb6c94791682fa6e9
kahuku <buzzinbubba@gmail.com>
2024-10-08 13:25:09 -0600

    donuts route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,3 +2,25 @@ def register_routes(app):
     @app.route('/')
     def index():
         return 'Hello, World!'
+
+    @app.route('/donuts', methods=['GET'])
+    def donuts():
+        return {
+            'donuts': [
+                {
+                    'id': 1,
+                    'name': 'Glazed',
+                    'price': 1.50
+                },
+                {
+                    'id': 2,
+                    'name': 'Chocolate',
+                    'price': 2.00
+                },
+                {
+                    'id': 3,
+                    'name': 'Boston Cream',
+                    'price': 2.50
+                }
+            ]
+        }
